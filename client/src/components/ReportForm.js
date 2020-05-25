53f9e4defb68a9e18f88d71e7cb0932443a67de0
Anubha Agrawal <anubhakannnu@gmail.com>
2020-05-25 23:56:01 +0530

    Add some CSS

--- client/src/components/ReportForm.js
+++ client/src/components/ReportForm.js
@@ -5,6 +5,7 @@ import Popover from '@material-ui/core/Popover';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Message from './Message';
+import '../App.css';
 
 const useStyles = makeStyles((theme) => ({
   typography: {
@@ -284,39 +285,47 @@ function ReportForm() {
       const id3 = open3 ? 'simple-popover' : undefined;
     return (
         <div>
+          <div className = "header">
              <h2>Report your Stolen Car</h2>
              <h4>Fill this Form</h4>
-            <form onSubmit = { handleSubmit}>
+             </div>
+            <form onSubmit = { handleSubmit} className ="form" >
+            <label for="fname" className="lab">User Name</label>
                 <div className = "form-input">
+               
                     <input type= "text" name = "name" placeholder = "User Name" value = {reportBody.username} onChange = {
                         (e)=> { console.log(e.target.value);
                             SetReport({...reportBody, username: e.target.value})
                                 }}>
                     </input>
                 </div>
+                <label for="fname" className ="lab">Age</label>
                 <div className = "form-input">
                     <input type= "text" name = "age" placeholder = "Age" value = {reportBody.age} onChange = {(e)=> SetReport({...reportBody, age: e.target.value})}>
                     </input>
                 </div>
-
+                <label for="fname" className = "lab">LicenceNumber</label>
                 <div className = "form-input">
                     <input type= "text" name = "LN" placeholder = "Licence Number" value = {reportBody.LicenceNumber} onChange = {(e)=> SetReport({...reportBody, LicenceNumber: e.target.value})}>
                     </input>
                 </div>
+                <label for="fname" className = "lab">AdharCard Number</label>
                 <div className = "form-input">
                     <input type= "text" name = "UserId" placeholder = "User Id(AdharCard Number) " value = {reportBody.UserID} onChange = {(e)=> SetReport({...reportBody, UserID: e.target.value})}>
                     </input>
                 </div> 
+                <label for="fname" className = "lab">Proof of Ownership</label>
                 <div className = "form-input">
                     <input type= "text" name = "poo" placeholder = "Proof of ownership" value = {reportBody.ProofofOwnerShip} onChange = {(e)=> SetReport({...reportBody, ProofofOwnerShip: e.target.value})}>
                     </input>
                 </div>
+                <label for="fname" className = "lab">VIN</label>
                 <div className = "form-input">
                     <input type= "text" name = "vin" placeholder = "VIN" value = {reportBody.VIN} onChange = {(e)=> SetReport({...reportBody, VIN: e.target.value})}>
                     </input>
                 </div>
 
-                <button>Submit</button>
+                <button className="btn">Submit</button>
             </form>
             {message1 ? <Message message = {message1}/> : null}
             {console.log(data)}
@@ -338,12 +347,12 @@ function ReportForm() {
           horizontal: 'center',
         }}
       >
-        <Typography className={classes.typography}>   {data.length!==0 && data.map((post, index) =>{
+        <Typography className={classes.typography}>   <ul>{data.length!==0 && data.map((post, index) =>{
                 return(<div key = {index}>
                    
-                <h3>{index} UserName- {post.username}, UserId- {post.UserID}, LicenceNumber- {post.LicenceNumber}, status- {post.status? "Assigned": "Not Assigned"}</h3>
+                <li> UserName- {post.username}, UserId- {post.UserID}, LicenceNumber- {post.LicenceNumber}, status- {post.status? "Assigned": "Not Assigned"}</li>
                 </div>)
-            })}
+            } )}</ul>
 </Typography>
       </Popover>
       </div>
@@ -371,7 +380,7 @@ function ReportForm() {
                     <input type= "text" name = "username" placeholder = "Add Unique UserName of Police Officer" value = {police.username} onChange = {(e)=> setPolice({...police, username: e.target.value})}>
                     </input>
                 </div>
-                <button>Submit</button>
+                <button className = "btn1">Submit</button>
         </form>
        
         {message ? <Message message = {message}/> : null}
@@ -397,13 +406,13 @@ function ReportForm() {
           horizontal: 'center',
         }}
       >
-        <Typography className={classes.typography}>     {getpolice.length!==0 && getpolice.map((post, index) =>{
+        <Typography className={classes.typography}>    <ul> {getpolice.length!==0 && getpolice.map((post, index) =>{
                 return(<div key = {index}>
                    
-                <h3>{index} UserName- {post.username} status- {post.assigntask? "true": "false"}</h3>
+                <li>UserName- {post.username}, Status- {post.assigntask? "true": "false"}</li>
                
                 </div>)
-            })}
+            })}</ul>
 </Typography>
       </Popover>
       </div>
@@ -434,7 +443,7 @@ function ReportForm() {
                     </input>
                    
                 </div>
-                <button>Submit</button>
+                <button className="btn1">Submit</button>
           </form>      
 </Typography>
       </Popover>
